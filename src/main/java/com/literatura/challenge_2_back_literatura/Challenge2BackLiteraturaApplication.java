8e36035b53282a4aa4730ef0f97cb4a5968c902f
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-06-06 15:23:05 -0500

    iniciando el reto

---
+++ src/main/java/com/literatura/challenge_2_back_literatura/Challenge2BackLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.literatura.challenge_2_back_literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge2BackLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge2BackLiteraturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/literatura/challenge_2_back_literatura/Challenge2BackLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literatura.challenge_2_back_literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge2BackLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
